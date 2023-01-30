e011ecdfa8900323234d9e71d116a4175938fa7e
Васильев Семён <s.vasilev@vipservice.ru>
2023-01-30 18:37:38 +0800

    Настраивает dev-server и доабвляет env переменные

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,15 +1,17 @@
 import path from 'path';
 import webpack from 'webpack';
+import { buildDevServer } from './buildDevServer';
 import { buildLoaders } from './buildLoaders';
 import { buildPlugins } from './buildPlugins';
 import { buildResolves } from './buildResolvers';
 import { BuildOptions } from './types/config';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-	const { paths, mode } = options
+	const { paths, mode, isDev } = options
 	return {
 		mode: mode,
 		entry: paths.entry,
+		devServer: isDev ? buildDevServer(options) : undefined,
 		output: {
 			clean: true,
 			path: paths.build,
@@ -19,6 +21,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
 			rules: buildLoaders()
 		},
 		resolve: buildResolves(),
-		plugins: buildPlugins(options)
+		plugins: buildPlugins(options),
+		devtool: isDev ? 'inline-source-map' : undefined,
 	}
 }
