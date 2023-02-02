bb1e66ed4df8b07444bf5fc532d2dbdff987e135
Васильев Семён <s.vasilev@vipservice.ru>
2023-02-02 20:34:13 +0800

    Настраивает сборку под css modules и подключает реакт

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,27 @@
+import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: "css-loader",
+				options: {
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module')),
+						localIdentName: options.isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]'
+					},
+				}
+			},
+			"sass-loader",
+		],
+	}
+
+	// Если без ts, то нужен babel-loader
 	const typescriptLoader = {
 		test: /\.tsx?$/,
 		use: 'ts-loader',
@@ -8,6 +29,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 	}
 
 	return [
-		typescriptLoader
+		typescriptLoader, cssLoader
 	]
 }
