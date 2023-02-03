f705b1317aefcf3321bfae2686905f4b4292eeeb
Васильев Семён <s.vasilev@vipservice.ru>
2023-02-03 18:03:37 +0800

    Добавляет темизацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import { AboutPageLazy } from './pages/about/AboutPage.lazy'
 import { MainPageLazy } from './pages/main/MainPage.lazy'
+import './styles/'
+import { useTheme } from './themes/useTheme'
 
 const App = () => {
+	const { theme, toggleTheme } = useTheme()
+
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
 			<Link to='/about'>about</Link>
 			<Link to='/'>main</Link>
-
+			<button onClick={toggleTheme}>toggle theme</button>
 			<Suspense fallback={<div>Loading...</div>}>
 				<Routes>
 					<Route path='/about' element={<AboutPageLazy />} />
 					<Route path='/' element={<MainPageLazy />} />
 				</Routes>
 			</Suspense>
-
 		</div>
 	)
 }
---
+++ src/themes/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, FC, useMemo, useState } from "react";
+
+export enum Theme {
+	LIGHT = 'light',
+	DARK = 'dark',
+}
+
+export interface ThemeContextProps {
+	theme?: Theme;
+	setTheme?: (theme: Theme) => void
+}
+
+export const ThemeContext = createContext<ThemeContextProps>({});
+
+export const LOCAL_STORAGE_THEME_KEY = 'theme';
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.DARK
+
+export const ThemeContextProvider: FC = ({ children }) => {
+	const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+
+	const defaultProps = useMemo(() => ({ theme, setTheme }), [theme])
+
+	return (
+		<ThemeContext.Provider value={defaultProps}>
+			{children}
+		</ThemeContext.Provider>
+	)
+}
