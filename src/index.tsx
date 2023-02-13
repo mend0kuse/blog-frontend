16c1d85d787e00cf16552f212af3fa5902b5828f
Васильев Семён <s.vasilev@vipservice.ru>
2023-02-13 14:13:50 +0800

    Добавляет обработчки ошибок - ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import App from 'app/App';
+import ErrorBoundary from 'app/providers/ErrorBoundary/ui/ErrorBoundary';
 import { ThemeContextProvider } from 'app/providers/ThemeProvider';
 import 'shared/config/i18n/i18n';
 
@@ -7,9 +8,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 render(
 	<BrowserRouter>
-		<ThemeContextProvider>
-			<App />
-		</ThemeContextProvider>
+		<ErrorBoundary>
+			<ThemeContextProvider>
+				<App />
+			</ThemeContextProvider>
+		</ErrorBoundary>
 	</BrowserRouter>,
 	document.getElementById('root'),
 );
