1f87252db89e055826b52fd5b03e0f557be4657e
mend0kuse <vasilievsam03@mail.ru>
2023-06-08 19:32:25 +0800

    Создает скрипт, который генерирует fsd слайсы

---
+++ scripts/createSlice/templates/componentTemplate.js
@@ -0,0 +1,23 @@
+const interfaceConst = 'interface';
+
+module.exports = (componentName) => `import cn from 'shared/lib/classNames/cn';
+import { useTranslation } from 'react-i18next';
+import styles from './${componentName}.module.scss';
+import { memo } from 'react';
+
+${interfaceConst} ${componentName}Props {
+    className?: string;
+}
+
+export const ${componentName} = memo((props: ${componentName}Props) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    
+    return (
+        <div className={classNames(styles.${componentName}, {}, className)}>
+           
+        </div>
+    );
+});
+
+${componentName}.displayName = ${componentName};`;
